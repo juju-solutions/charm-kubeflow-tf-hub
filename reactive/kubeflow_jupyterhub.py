edb7e7c6367a8c5acfe86e217fb0a89bbd8d2cf5
Kenneth Koski <kenneth.koski@canonical.com>
2019-02-25 17:35:35 +0100

    Update service name
    A few references to the old tf-hub naming scheme
    were left.

--- reactive/kubeflow_jupyterhub.py
+++ reactive/kubeflow_jupyterhub.py
@@ -8,7 +8,7 @@ from charms.reactive import when, when_not, when_any
 from charms import layer
 
 
-@when('charm.kubeflow-tf-hub.started')
+@when('charm.kubeflow-jupyterhub.started')
 def charm_ready():
     layer.status.active('')
 
@@ -16,11 +16,11 @@ def charm_ready():
 @when_any('layer.docker-resource.jupyterhub-image.changed',
           'config.change')
 def update_image():
-    clear_flag('charm.kubeflow-tf-hub.started')
+    clear_flag('charm.kubeflow-jupyterhub.started')
 
 
 @when('layer.docker-resource.jupyterhub-image.available')
-@when_not('charm.kubeflow-tf-hub.started')
+@when_not('charm.kubeflow-jupyterhub.started')
 def start_charm():
     layer.status.maintenance('configuring container')
 
@@ -37,7 +37,7 @@ def start_charm():
     layer.caas_base.pod_spec_set({
         'containers': [
             {
-                'name': 'tf-hub',
+                'name': 'jupyterhub',
                 'imageDetails': {
                     'imagePath': image_info.registry_path,
                     'username': image_info.username,
@@ -81,4 +81,4 @@ def start_charm():
     })
 
     layer.status.maintenance('creating container')
-    set_flag('charm.kubeflow-tf-hub.started')
+    set_flag('charm.kubeflow-jupyterhub.started')
